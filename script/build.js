ff59da26e0b12c152480474e8e2d08fb23418822
wenps <2534491497@qq.com>
2025-03-04 16:31:19 +0800

    feat: 补充构建脚本文档

--- script/build.js
+++ script/build.js
@@ -1,16 +1,18 @@
 /*
  * @Date: 2024-12-07 16:03:52
  * @LastEditors: xiaoshan
- * @LastEditTime: 2025-02-06 19:10:52
+ * @LastEditTime: 2025-02-28 10:48:14
  * @FilePath: /element-tag-marker/script/build.js
  */
 // @ts-check
 import shell from 'shelljs'; // 使用 import 引入 shelljs 模块
 import { select } from '@inquirer/prompts'; // 使用 import 引入 select 函数
 
+// 解析命令行参数并将其存储在 Map 中
 const parseArgsToMap = () => {
     const args = new Map()
 
+    // 遍历命令行参数，将每个参数按 '=' 分割并存入 Map
     process.argv.forEach(arg => {
         const [key, value] = arg.split('=')
         args.set(key, value)
@@ -24,22 +26,29 @@ const argMap = parseArgsToMap();
 const run = async () => {
     // 自带指令 d 标识开发模式
     const isDev = argMap.has('d')
+    
+    // 定义构建命令的函数
     const runBuild = () => {
-        const buildCmd = 'pnpm build' + (isDev ? ' -w' : '')
-        shell.exec(buildCmd, { async: isDev })
+        const buildCmd = 'pnpm build' + (isDev ? ' -w' : '') // 根据是否为开发模式决定构建命令
+        shell.exec(buildCmd, { async: isDev }) // 执行构建命令
     }
 
+    // 定义可选的插件类型
     const choices = ['vite', 'webpack'].map(type => {
         return {
             name: type,
             value: type + 'ElementTagMarkerPlugin' // 这里用了拼接，要留意后续目录是否变更
         }
     })
+    
     let dir
     // 自带指令 p 标识指定插件类型
     if (argMap.has('p')) {
+        // 如果指定了插件类型，则从 choices 中查找对应的值
         dir = choices.find(choice => choice.name === argMap.get('p'))?.value
     }
+    
+    // 如果没有指定插件类型，则通过选择提示用户选择
     if (!dir) {
         dir = await select({
             message: 'please select plugin type ——',
@@ -47,14 +56,17 @@ const run = async () => {
             default: choices[0].value
         })
     }
+    
+    // 进入核心插件目录
     shell.cd('packages/elementTagMarkerCore')
-    runBuild()
+    runBuild() // 执行构建
 
-    shell.cd('..')
+    shell.cd('..') // 返回上级目录
 
+    // 复制 README 文件到指定插件目录
     shell.cp('readme*', dir)
-    shell.cd(dir)
-    runBuild()
+    shell.cd(dir) // 进入指定插件目录
+    runBuild() // 执行构建
 }
 
-run()
+run() // 启动构建流程
